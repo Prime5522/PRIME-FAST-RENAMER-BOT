0aa352ac1ea07c192a4d1c02d5f338bcb599f6d4
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-10-26 12:41:09 +0600

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -12,23 +12,25 @@ Price 0
 
 **🪙 Basic**
 Daily  Upload  limit 20GB
-Price Rs 49  ind /🌎 0.59$  per Month
+Price 50₹  /🌎 0.59$  per Month
 
 **⚡ Standard**
 Daily Upload limit 50GB
-Price Rs 99  ind /🌎 1.19$  per Month
+Price 100₹ /🌎 1.19$  per Month
 
 **💎 Pro**
 Daily Upload limit 100GB
-Price Rs 179  ind /🌎 2.16$  per Month
+Price 150₹  /🌎 2.16$  per Month
 
 Payment Details :-
-<b>➜ UPI ID :</b> <code>RahulReviews@UPI</code>
+<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a>
 
-After Payment Send Screenshots Of Payment To Admin @CodeXBro"""
+💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে
+ পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে
+𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> /n👇(Admin)👇Send Payment Receipt 🧾 Screenshot"""
     
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url = "https://telegram.me/CodeXBro"),
+        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/Prime_Admin_Support_BOT"),
         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
         ])
     
