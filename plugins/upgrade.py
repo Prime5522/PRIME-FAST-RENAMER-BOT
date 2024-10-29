56fe1b443efb576bf6903e4671e0d572a6fe9746
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-10-29 21:32:49 +0600

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -1,8 +1,5 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
-from pyrogram import Client , filters
-
-
-
+from pyrogram import Client, filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot, update):
@@ -40,39 +37,44 @@ Payment Details:
         photo=photo_url,
         caption=text,
         reply_markup=keybord,
-        parse_mode="html",
-        disable_web_page_preview=True
+        parse_mode="html"
     )
-    
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
-async def upgradecm(bot,message):
+async def upgradecm(bot, message):
     text = """**Free Plan User**
-Daily  Upload limit 5GB
+Daily Upload limit 5GB
 Price 0
 
 **🪙 Basic**
-Daily  Upload  limit 20GB
-Price 50₹  /🌎 0.59$  per Month
+Daily Upload limit 20GB
+Price 50₹ /🌎 0.59$ per Month
 
 **⚡ Standard**
 Daily Upload limit 50GB
-Price 100₹ /🌎 1.19$  per Month
+Price 100₹ /🌎 1.19$ per Month
 
 **💎 Pro**
 Daily Upload limit 100GB
-Price 150₹  /🌎 2.16$  per Month
+Price 150₹ /🌎 2.16$ per Month
 
-Payment Details :-
-<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a>
+Payment Details:
+<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a></b>
 
-💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে
- পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে
-𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> /n👇(Admin)👇Send Payment Receipt 🧾 Screenshot"""
-    
+💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. 
+যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে
+𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> 
+👇(Admin)👇Send Payment Receipt 🧾 Screenshot"""
+
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/Prime_Admin_Support_BOT"),
-        InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
-        ])
-    
-    await message.reply_text(text=text, reply_markup=keybord, quote=True, disable_web_page_preview=True)
+        [InlineKeyboardButton("🦋 Admin", url="https://t.me/Prime_Admin_Support_BOT"),
+         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
+    ])
+
+    await message.reply_text(
+        text=text,
+        reply_markup=keybord,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        quote=True
+    )
