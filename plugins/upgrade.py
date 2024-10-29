c7b7bb4d7b619d6330550ef532b630df6ce21a47
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-10-30 05:40:48 +0600

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -1,9 +1,11 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
+from pyrogram import Client , filters
+
+
+
 
 @Client.on_callback_query(filters.regex('upgrade'))
-async def upgrade(bot, update):
-    photo_url = "https://envs.sh/AR9.jpg"  # এখানে আপনার ছবির লিংক দিন
+async def upgrade(bot,update):
     text = """<b>Free Plan User</b>
 Daily Upload limit 5GB
 Price 0
@@ -25,56 +27,46 @@ Payment Details:
 
 💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. 
 যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে 
-𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> 
-👇(Admin)👇Send Payment Receipt 🧾 Screenshot"""
-
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url="https://t.me/Prime_Admin_Support_BOT"),
-         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
-    ])
-
-    await update.message.reply_photo(
-        photo=photo_url,
-        caption=text,
-        reply_markup=keyboard,
-        parse_mode="html"  # এটি সঠিক মোডে রাখতে হবে
-    )
+𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> \n\n
+👇( 𝗔𝗗𝗠𝗜𝗡 )👇Send Payment Receipt 🧾 Screenshot"""
+    
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/Prime_Admin_Support_BOT"),
+        InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
+        ])
+    
+    await update.message.edit(text = text,reply_markup = keybord, disable_web_page_preview=True)
+    
+    
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
-async def upgradecm(bot, message):
-    text = """<b>Free Plan User</b>
-Daily Upload limit 5GB
+async def upgradecm(bot,message):
+    text = """**Free Plan User**
+Daily  Upload limit 5GB
 Price 0
 
-<b>🪙 Basic</b>
-Daily Upload limit 20GB
-Price 50₹ /🌎 0.59$ per Month
+**🪙 Basic**
+Daily  Upload  limit 20GB
+Price 50₹  /🌎 0.59$  per Month
 
-<b>⚡ Standard</b>
+**⚡ Standard**
 Daily Upload limit 50GB
-Price 100₹ /🌎 1.19$ per Month
+Price 100₹ /🌎 1.19$  per Month
 
-<b>💎 Pro</b>
+**💎 Pro**
 Daily Upload limit 100GB
-Price 150₹ /🌎 2.16$ per Month
+Price 150₹  /🌎 2.16$  per Month
 
-Payment Details:
-<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a></b>
+Payment Details :-
+<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a>
 
-💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. 
-যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে
-𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> 
-👇(Admin)👇Send Payment Receipt 🧾 Screenshot"""
-
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url="https://t.me/Prime_Admin_Support_BOT"),
-         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
-    ])
-
-    await message.reply_text(
-        text=text,
-        reply_markup=keyboard,
-        parse_mode="html",  # এটি সঠিক মোডে রাখতে হবে
-        disable_web_page_preview=True,
-        quote=True
-    )
+💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে
+ পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে
+𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> \n\n👇(Admin)👇Send Payment Receipt 🧾 Screenshot"""
+    
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/Prime_Admin_Support_BOT"),
+        InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
+        ])
+    
+    await message.reply_text(text=text, reply_markup=keybord, quote=True, disable_web_page_preview=True)
