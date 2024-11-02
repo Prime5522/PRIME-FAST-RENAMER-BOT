c340ffcdd19327f329172a20ef9d8e50d026109e
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-11-02 09:50:52 +0600

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -4,20 +4,20 @@ from pyrogram import Client, filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot, update):
     text = """<b>Free Plan User</b>
-Daily Upload limit 5GB
-Price 0
+Daily Upload limit: 5GB
+Price: 0
 
 <b>🪙 Basic</b>
-Daily Upload limit 20GB
-Price 50₹ /🌎 0.59$ per Month
+Daily Upload limit: 20GB
+Price: 50₹ /🌎 0.59$ per Month
 
 <b>⚡ Standard</b>
-Daily Upload limit 50GB
-Price 100₹ /🌎 1.19$ per Month
+Daily Upload limit: 50GB
+Price: 100₹ /🌎 1.19$ per Month
 
 <b>💎 Pro</b>
-Daily Upload limit 100GB
-Price 150₹ /🌎 2.16$ per Month
+Daily Upload limit: 100GB
+Price: 150₹ /🌎 2.16$ per Month
 
 Payment Details:
 <b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a></b>
@@ -28,9 +28,9 @@ Payment Details:
 👇Send Payment Receipt 🧾 Screenshot"""
     
     keyboard = InlineKeyboardMarkup([
-    [InlineKeyboardButton("🦋 Click Here To Send Payment Receipt 🧾", url="https://t.me/Prime_Admin_Support_BOT")],
-    [InlineKeyboardButton("✖️ Cancel ✖️", callback_data="cancel")]
-])
+        [InlineKeyboardButton("🦋 Click Here To Send Payment Receipt 🧾", url="https://t.me/Prime_Admin_Support_BOT")],
+        [InlineKeyboardButton("✖️ Cancel ✖️", callback_data="cancel")]
+    ])
     
     await bot.send_photo(
         chat_id=update.message.chat.id,
@@ -41,24 +41,24 @@ Payment Details:
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot, message):
-    text = """**Free Plan User**
-Daily Upload limit 5GB
-Price 0
+    text = """<b>Free Plan User</b>
+Daily Upload limit: 5GB
+Price: 0
 
-**🪙 Basic**
-Daily Upload limit 20GB
-Price 50₹ /🌎 0.59$ per Month
+<b>🪙 Basic</b>
+Daily Upload limit: 20GB
+Price: 50₹ /🌎 0.59$ per Month
 
-**⚡ Standard**
-Daily Upload limit 50GB
-Price 100₹ /🌎 1.19$ per Month
+<b>⚡ Standard</b>
+Daily Upload limit: 50GB
+Price: 100₹ /🌎 1.19$ per Month
 
-**💎 Pro**
-Daily Upload limit 100GB
-Price 150₹ /🌎 2.16$ per Month
+<b>💎 Pro</b>
+Daily Upload limit: 100GB
+Price: 150₹ /🌎 2.16$ per Month
 
-Payment Details :-
-<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a>
+Payment Details:
+<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a></b>
 
 💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. 
 যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে 
@@ -75,4 +75,4 @@ Payment Details :-
         photo="https://envs.sh/AR9.jpg",
         caption=text,
         reply_markup=keyboard
-    )
+                  )
