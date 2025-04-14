b7fe0b47e1b07e6c52e70e91e86cdf0823286646
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-04-14 17:56:41 +0600

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002184630716"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://shnayembd1:XAuVBX2Se3iFSnwa@cluster0.rha6xvq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 
